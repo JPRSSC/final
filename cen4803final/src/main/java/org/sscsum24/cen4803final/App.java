0f86685e5f8ca4ad22b12a3dc2efb3da0127a214
joshr <joshr@host.docker.internal>
2024-04-21 12:43:28 -0400

    Created project.

---
+++ cen4803final/src/main/java/org/sscsum24/cen4803final/App.java
@@ -0,0 +1,13 @@
+package org.sscsum24.cen4803final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
