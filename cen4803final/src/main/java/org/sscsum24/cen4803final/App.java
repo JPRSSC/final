855b04190ac14a3abc2063baf0c0dd9d7b032d74
joshr <joshr@host.docker.internal>
2024-04-21 22:44:01 -0400

    This commit is for the final presentation.

--- cen4803final/src/main/java/org/sscsum24/cen4803final/App.java
+++ cen4803final/src/main/java/org/sscsum24/cen4803final/App.java
@@ -4,11 +4,10 @@ package org.sscsum24.cen4803final;
  * This is a simple program that calculates the sum
  * of two numbers.
  * 
- * This is demonstrating GitHub integration.
  */
 public class App 
 {
 	public int add (int numberA, int numberB) {
-		return numberA + numberB;
+		return numberA - numberB;
 	}
 }
