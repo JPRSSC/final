cc52417e39202565005aafd4af2bc01ba2875221
joshr <joshr@host.docker.internal>
2024-04-21 22:37:28 -0400

    Presentation

--- cen4803final/src/main/java/org/sscsum24/cen4803final/App.java
+++ cen4803final/src/main/java/org/sscsum24/cen4803final/App.java
@@ -3,6 +3,8 @@ package org.sscsum24.cen4803final;
 /**
  * This is a simple program that calculates the sum
  * of two numbers.
+ * 
+ * This is demonstrating GitHub integration.
  */
 public class App 
 {
